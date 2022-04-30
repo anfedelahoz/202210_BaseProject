e2327f26d5f5d66a829910b8c77134fc8980f644
anfedelahoz <andelahozg@gmail.com>
2022-04-29 23:05:08 -0500

    Funcionalidades terminadas

---
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -0,0 +1,72 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CafeListComponent } from './cafe-list.component';
+import { CafeService } from '../cafe.service';
+import { Cafe } from '../cafe';
+
+describe('CafeListComponent', () => {
+  let component: CafeListComponent;
+  let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CafeListComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [CafeService]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CafeListComponent);
+    component = fixture.componentInstance;
+
+    component.cafes = [
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()),
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()),
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl())
+    ]
+
+    fixture.detectChanges();
+    debug = fixture.debugElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('component should have 3 new more row elements', () => {
+    expect(debug.query(By.css("tbody")).children.length).toEqual(component.cafes.length);
+  });
+
+  it("component has a header table", () => {
+    expect(debug.query(By.css("thead")).children.length).toEqual(1);
+  });
+});
---
+++ src/app/cafe/cafe.service.spec.ts
@@ -0,0 +1,18 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { CafeService } from './cafe.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('Service: Cafe', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CafeService],
+      imports: [HttpClientTestingModule]
+    });
+  });
+
+  it('should inject service', inject([CafeService], (service: CafeService) => {
+    expect(service).toBeTruthy();
+  }));
+});
